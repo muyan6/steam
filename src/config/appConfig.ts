ba81df176ce7c929df9182da250b9b26621d85ac
muyan6 <3142755779@qq.com>
2026-09-04 12:12:44 +0800

    feat(config): update API_BASE_URL to cloud backend http://150.158.129.222:1257

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -2,17 +2,12 @@
  * SteamMaster 商业版应用核心配置
  */
 
-const isDev = process.env.NODE_ENV !== 'production' || !process.env.NODE_ENV;
-
 export const APP_CONFIG = {
   APP_NAME: 'SteamMaster',
   VERSION: '1.0.0',
   
   // 云端官方服务器地址
-  // 开发模式下自动走 127.0.0.1:1257，编译打包生产环境走云端服务器地址
-  API_BASE_URL: isDev 
-    ? 'http://127.0.0.1:1257' 
-    : 'http://127.0.0.1:1257', // 部署到服务器后，在此填入你的正式服务器公网 IP 或域名，如 'http://1.2.3.4:1257'
+  API_BASE_URL: 'http://150.158.129.222:1257',
 
   REQUEST_TIMEOUT_MS: 8000
 };
